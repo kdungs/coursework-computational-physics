3f4e7b40b9bb5569dec3e5c8dd9e22b28cdb177e
Kevin Dungs <kevin@kads24.eu>
2014-04-08 18:39:04 +0200

    Optimise plotting. Remove duplicate points to speed up plotting and reduce file size.

--- src/00/bifurcation_diagram.py
+++ src/00/bifurcation_diagram.py
@@ -7,7 +7,7 @@ from os import listdir
 files = listdir('./data/')
 for f in files:
     r = float(f.replace('.txt', ''))
-    xN = np.loadtxt('./data/{}'.format(f))
+    xN = np.unique(np.loadtxt('./data/{}'.format(f)))
     plt.plot([r] * len(xN), xN, 'k.')
 plt.xlabel('$r$')
 plt.ylabel('$x_N$')
