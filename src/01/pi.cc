eae2cac172715bfb98ec27ac3eb0f21d5ea4c367
Kevin Dungs <kevin@kads24.eu>
2014-04-17 10:14:08 +0200

    Final version of the π approximation. I performed [a benchmark](https://gist.github.com/kdungs/10963341) and found out that the loop-based version is slightly faster (clang-503.0.40) so we go with that one. It is also more memory efficient.

--- src/01/pi.cc
+++ src/01/pi.cc
@@ -1,37 +1,33 @@
 /** @file.cc
- * Fast (but memory consuming) C++11 implementation of π approximation
+ * π approximation
  */
-#include <algorithm>
 #include <iostream>
 #include <random>
-#include <utility>
-#include <vector>
-
-typedef float Number;
-typedef std::pair<Number, Number> Point;
-typedef std::mt19937 RandomGenerator;
 
 /** Approximate π in a number of runs.
  * @param N the number of runs
  * @param rng the random number generator used
  * @return an approximation of π
  */
-Number ApproximatePi(const size_t N, RandomGenerator &rng) {
-  std::uniform_real_distribution<Number> dist(0.0, 1.0);
-  std::vector<Point> v(N);
-  std::generate(std::begin(v), std::end(v), [&]() {
-    return Point{dist(rng), dist(rng)};
-  });
-  size_t n_inside = std::count_if(std::begin(v), std::end(v),
-    [](const Point &p) { return p.first * p.first + p.second * p.second <= 1; }
-  );
-  return 4.0 * n_inside / N;
+double ApproximatePi(const unsigned long N, std::mt19937 &rng) {
+  static std::uniform_real_distribution<double> dist(0.0, 1.0);
+  unsigned long i,
+                n = 0;
+  double x,
+         y;
+  for (i = 0; i < N; i++) {
+    x = dist(rng);
+    y = dist(rng);
+    if (x * x + y * y <= 1) {
+      ++n;
+    }
+  }
+  return 4.0 * n / N;
 }
 
-/** Starting point.
- */
+/** Main. */
 int main(int argc, char *argv[]) {
   std::random_device rd;
-  RandomGenerator rng(rd());
-  std::cout << "π = " << ApproximatePi(1000000, rng) << std::endl;
+  std::mt19937 rng(rd());
+  std::cout << "π = " << ApproximatePi(10000000, rng) << std::endl;
 }
