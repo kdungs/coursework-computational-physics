c15ec73e54aa629a825ba773a0c94530869618bc
Kevin H <kevin@kehei.de>
2014-04-24 22:51:20 +0200

    add labels

--- src/02/fit_p.py
+++ src/02/fit_p.py
@@ -29,6 +29,9 @@ for f in files:
     plt.xlabel('$p$')
     plt.ylabel('$N/N_{max}$')
     plt.legend(['Data', 'Fit'], 'best')
+    plt.text(0.3, 0.7,
+             '$p_c = {:.4f}\pm{:.4f}$'.format(coeff[1],
+                                              np.sqrt(coeff_var[1][1])))
     plt.xlim([min(p) * 0.9, max(p) * 1.1])
     plt.ylim([0, max(p_N) * 1.1])
     plt.savefig('./plots/' + f.replace('txt', 'pdf'))
@@ -36,13 +39,17 @@ for f in files:
 
 p_c_func = lambda x, a, b: a/x + b
 coeff, coeff_var = curve_fit(p_c_func, sizes, p_c)
-print('{:.4f}({:.4f})'.format(coeff[1], np.sqrt(coeff_var[1][1])))
+# print('{:.4f}({:.4f})'.format(coeff[1], np.sqrt(coeff_var[1][1])))
 
-sizearr = np.arange(min(sizes), max(sizes), 0.001)
+sizearr = np.arange(0.01, max(sizes), 0.001)
 plt.errorbar(sizes, p_c, yerr=p_c_err, fmt='kx')
 plt.plot(sizearr, p_c_func(sizearr, coeff[0], coeff[1]), 'r-')
-plt.xlim([0.9*min(sizes), 1.1*max(sizes)])
+plt.xlim([-10, 1.1*max(sizes)])
+plt.ylim([0.5, 0.61])
 plt.xlabel('$N$')
 plt.ylabel('$p_c$')
+plt.text(100, 0.56,
+         '$p_c = {:.4f}\pm{:.4f}$'.format(coeff[1],
+                                          np.sqrt(coeff_var[1][1])))
 plt.savefig('./plots/compare.pdf')
 plt.clf()
