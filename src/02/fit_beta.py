05433dacae9b79c2cd71a80ab2305db7108c8298
kevin.heinicke <kevin.heinicke@tu-dortmund.de>
2014-04-26 14:33:55 +0200

    tuning

--- src/02/fit_beta.py
+++ src/02/fit_beta.py
@@ -12,8 +12,9 @@ for f in files:
     pFit = p[:pIndex[0][0]]
     maxSizeFit = maxSize[:pIndex[0][0]]
 
-    func = lambda x, a, b: a * abs(x-0.597)**b
-    coeff, coeff_var = curve_fit(func, pFit, maxSizeFit, p0=[1.0, -0.1], maxfev=10000)
+    func = lambda x, a, b: a * abs(x - 0.597) ** (-b)
+    coeff, coeff_var = curve_fit(func, pFit, maxSizeFit,
+                                 p0=[1.0, -0.1], maxfev=10000)
 
     plt.errorbar(p,
                  maxSize,
@@ -25,7 +26,7 @@ for f in files:
     plt.legend(['Data', 'Fit'], 'best')
     plt.text(0.2, 0.7 * max(maxSize),
              '$\\beta = {:.4f}\pm{:.4f}$'.format(coeff[1],
-                                              np.sqrt(coeff_var[1][1])))
+                                                 np.sqrt(coeff_var[1][1])))
     plt.ylim([0, max(maxSize)])
     plt.savefig('./plots/beta/' + f.replace('txt', 'pdf'))
     plt.clf()
