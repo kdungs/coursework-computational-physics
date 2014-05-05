e193ef96fc920897dff74c72f730a9a05c2cf049
Kevin Dungs <kevin@kads24.eu>
2014-05-05 16:39:57 +0200

    First steps for sheet no 4.

---
+++ src/04/09/analytical.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import gamma
+
+def V(d):
+    return np.pi ** (d / 2) / gamma(1 + d / 2)
+
+ds = np.linspace(1, 200)
+plt.plot(ds, V(ds))
+plt.xlabel('$d$')
+plt.ylabel('$V_S(d)$')
+plt.yscale('log')
+plt.minorticks_on()
+plt.savefig('analytical.pdf')
+
+print('V_S(200) = {}'.format(V(200)))
