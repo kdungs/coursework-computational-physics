6e209dddcd3f9925993bb631cb24fd1df969b2a2
Kevin Dungs <kevin@kads24.eu>
2014-05-15 23:06:25 +0200

    Minimal fixes.

--- src/05/decoupled.py
+++ src/05/decoupled.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-# theoretical
+# calculate theoretical value
 N = 100 * 100
 beta = 1
 Jx, Jy = 1, 0
 lambda_p = np.exp(beta * Jx) + np.exp(-beta * Jx)
 lambda_m = np.exp(beta * Jx) - np.exp(-beta * Jx)
-
 E = -N * np.sinh(beta) / np.cosh(beta)
-
 print(E)
+
+# make nice plot
+sc = np.loadtxt('decoupled.txt', unpack=True)
+plt.pcolor(sc, cmap=plt.cm.copper)
+plt.savefig('decoupled.png')
--- src/05/snapshots.py
+++ src/05/snapshots.py
@@ -4,7 +4,7 @@ from itertools import product
 import numpy as np
 import matplotlib.pyplot as plt
 
-for kbT, c in product(['1', '2.25', '3', '50', '1000', 'decoupled'],
+for kbT, c in product(['1', '2.25', '3', '50', '1000'],
                       ['u', 'r']):
     sc = np.loadtxt('sc_{}_{}.txt'.format(c, kbT), unpack=True)
     plt.pcolor(sc, cmap=plt.cm.copper)
