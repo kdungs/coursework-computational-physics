94916bd50b4ca63bd30470130b59e2d544ec7a0c
Kevin Dungs <kevin@kads24.eu>
2014-05-21 17:16:50 +0200

    Make stuff work.

--- src/06/main.cc
+++ src/06/main.cc
@@ -1,3 +1,19 @@
-#include "metropolis.hpp"
+#include <iostream>
+#include <random>
 
-int main(int argc, char *argv[]) {}
+#include "metropolis.hpp"
+#include "runningstats.hpp"
+
+int main(int argc, char *argv[]) {
+  std::random_device rd;
+  std::mt19937 rng(rd());
+
+  const double T = 3;
+  const size_t L = 100;
+  auto calc_C = HeatCapacityCalculator(T, L);
+  RunningStats<> C;
+  for (int i = 0; i < 10; i++) {
+    C.Push(metropolis(T, L, 100000, 1000, calc_C, rng));
+  }
+  std::cout << "C_\\nu = " << C.Mean() << " ± " << C.StdDev() << std::endl;
+}
--- src/06/metropolis.hpp
+++ src/06/metropolis.hpp
@@ -6,17 +6,49 @@
 #include <random>
 #include <vector>
 
+#include "runningstats.hpp"
+
+
 typedef short Spin;
 typedef std::vector<Spin> SpinConfiguration;
 typedef std::function<double(const SpinConfiguration&,
                              const double)> QuantityCalculator;
 
+
+struct HeatCapacityCalculator {
+ private:
+  const double T_sq_;
+  const size_t L_sq_;
+  RunningStats<> energy_,
+                 energy_squared_;
+ public:
+  HeatCapacityCalculator(const double temperature, const size_t side_length)
+   : T_sq_(temperature * temperature), L_sq_(side_length * side_length) {}
+  double operator() (const SpinConfiguration &sc, const double hamiltonian) {
+    energy_.Push(hamiltonian);
+    energy_squared_.Push(hamiltonian * hamiltonian);
+    const auto E = energy_.Mean();
+    return 1. / T_sq_ / L_sq_ * (energy_squared_.Mean() - E * E);
+  }
+};
+
+struct MagnetisationCalculator {
+ private:
+  RunningStats<> magnetisation;
+ public:
+  double operator() (const SpinConfiguration &sc, const double hamiltonian) {
+    // TODO: Implement
+    return 0;
+  }
+};
+
+
 template <typename URNG>
 SpinConfiguration randomConfiguration(const size_t side_length, URNG &rng) {
-  static std::uniform_real_distribution<double> dist(0, 1);
+  static std::uniform_int_distribution<Spin> dist(0, 1);
   SpinConfiguration sc(side_length * side_length);
   std::generate(std::begin(sc), std::end(sc), [&] () {
-    if (dist(rng) < .5) {
+    if (dist(rng) == 1) {
       return 1;  // up
     }
     return -1;  // down
@@ -24,7 +56,7 @@ SpinConfiguration randomConfiguration(const size_t side_length, URNG &rng) {
   return sc;  // use return value optimisation
 }
 
-double calculateEnergy(
+double calculateHamiltonian(
   const SpinConfiguration &sc,
   const double coupling_x=1,
   const double coupling_y=1
@@ -47,32 +79,123 @@ double calculateEnergy(
   return -energy;
 }
 
-double calculateEnergyDifference(
+double calculateHamiltonianDifference(
   const SpinConfiguration &sc,
   const size_t pos,
   const double coupling_x=1,
   const double coupling_y=1
 ) {
-  const size_t side_length = std::sqrt();
+const size_t L = std::sqrt(sc.size()),
+               x = pos % L,
+               y = pos / L;
+  Spin top,
+       right,
+       bottom,
+       left;
+  if (y > 0) {
+    top = sc[pos - L];
+  } else {
+    top = sc[x + (L - 1) * L];
+  }
+  if (x < L - 1) {
+    right = sc[pos + 1];
+  } else {
+    right = sc[pos + 1 - L];
+  }
+  if (y < L - 1) {
+    bottom = sc[pos + L];
+  } else {
+    bottom = sc[x];
+  }
+  if (x > 0) {
+    left = sc[pos - 1];
+  } else {
+    left = sc[pos - 1 + L];
+  }
+
+  return 2 * sc[pos] * (coupling_y * top + coupling_x * right
+                      + coupling_y * bottom + coupling_x * left);
 }
+
+
+template <typename URNG>
+double metropolisStep(
+  const double temperature,
+  const double hamiltonian_before,
+  const size_t pos,
+  SpinConfiguration &sc,
+  URNG &rng,
+  const double coupling_x,
+  const double coupling_y
+) {
+  static std::uniform_real_distribution<double> dist_p(0, 1);
+  const double diff = calculateHamiltonianDifference(sc, pos, coupling_x,
+                                                     coupling_y);
+  if (diff > 0 && dist_p(rng) > std::exp(-1. / temperature * diff)) {
+    return hamiltonian_before;  // reject flip
+  }
+  sc[pos] *= -1;
+  return hamiltonian_before + diff;
+}
+
+
+template <typename URNG>
+double metropolisRandomStep(
+  const double temperature,
+  const double hamiltonian_before,
+  SpinConfiguration &sc,
+  URNG &rng,
+  const double coupling_x,
+  const double coupling_y
+) {
+  std::uniform_int_distribution<size_t> dist_pos(0, sc.size() - 1);
+  return metropolisStep(temperature, hamiltonian_before, dist_pos(rng), sc,
+                        rng, coupling_x, coupling_y);
+}
+
+
+template <typename URNG>
+double metropolisSweep(
+  const double temperature,
+  const double hamiltonian_before,
+  SpinConfiguration &sc,
+  URNG &rng,
+  const double coupling_x,
+  const double coupling_y
+) {
+  double hamiltonian = hamiltonian_before;
+  for (size_t pos = 0; pos < sc.size(); pos++) {
+    hamiltonian = metropolisStep(temperature, hamiltonian, pos, sc, rng,
+                                 coupling_x, coupling_y);
+  }
+  return hamiltonian;
+}
+
 
 template <typename URNG>
 double metropolis(
   const double temperature,
   const size_t side_length,
-  const unsigned long num_steps_burnin,
-  const unsigned long num_sweeps,
+  const unsigned long num_sweeps_burnin,
+  const unsigned long num_sweeps_measurement,
   QuantityCalculator calculate_quantity,
   URNG &rng,
   const double coupling_x=1,
   const double coupling_y=1
 ) {
   SpinConfiguration sc = randomConfiguration(side_length, rng);
-  double energy = calculateEnergy(sc);
-  for (unsigned long i = 0; i < num_steps_burnin; i++) {
-    energy = metropolisStep(temperature, energy, sc, rng, coupling_x,
-                            coupling_y);
+  double hamiltonian = calculateHamiltonian(sc);
+  for (unsigned long i = 0; i < num_sweeps_burnin; i++) {
+    hamiltonian = metropolisSweep(temperature, hamiltonian, sc, rng,
+                                  coupling_x, coupling_y);
   }
+  double quantity = calculate_quantity(sc, hamiltonian);
+  for (unsigned long i = 0; i < num_sweeps_measurement; i++) {
+    hamiltonian = metropolisSweep(temperature, hamiltonian, sc, rng,
+                                  coupling_x, coupling_y);
+    quantity = calculate_quantity(sc, hamiltonian);
+  }
+  return quantity;
 }
 
 #endif  // __METROPOLIS_HPP__
