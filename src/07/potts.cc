409ebb387c8e4732153049a8926b00bb3d61aae9
Kevin Dungs <kevin@kads24.eu>
2014-05-29 18:03:28 +0200

    Some work.

--- src/07/potts.cc
+++ src/07/potts.cc
@@ -1,22 +1,47 @@
 #include <algorithm>
-#include <chrono>
+#include <cmath>
 #include <random>
 #include <vector>
 
+#include "union_find.hpp"
+
+typedef unsigned short Spin;
+typedef std::vector<Spin> SpinConfiguration;
+
+
 template <typename URNG>
-std::vector<unsigned int> RandomPotts(const size_t L, const size_t q, URNG &rng) {
-  std::uniform_int_distribution<unsigned int> dist_q(0, q - 1);
+SpinConfiguration RandomPotts(const size_t L, const size_t q, URNG &rng) {
+  std::uniform_int_distribution<Spin> dist_q(0, q - 1);
   auto random_q = std::bind(dist_q, rng);
-  std::vector<unsigned int> grid(L * L);
+  SpinConfiguration grid(L * L);
   std::generate(std::begin(grid), std::end(grid), random_q);
   return grid;
 }
 
-double calculateH() { return 0; }
+double calculateH(const SpinConfiguration &sc) {
+  const size_t L = std::sqrt(sc.size());
+  double H = 0;
+  size_t right, bottom;
+
+  for (size_t pos = 0; pos < sc.size(); pos++) {
+    right = pos + 1;
+    bottom = pos + L;
+    if ((pos + 1) % L == 0) {
+      right -= L;
+    }
+    if ((pos + L) >= L * L) {
+      bottom = pos % L;
+    }
+
+
+  }
+
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
   std::random_device rd;
   std::mt19937 rng(rd());
 
-  std::vector<unsigned int> grid = RandomPotts(100, 3, rng);
+  SpinConfiguration grid = RandomPotts(100, 3, rng);
 }
